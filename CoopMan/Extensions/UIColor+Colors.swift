5281d500f22f7952159018b4997c68d3e8bf112f
Francis Breidenbach <fbreidenbach@gmail.com>
2018-12-10 16:14:06 -0800

    Create UIColor extension.

---
+++ CoopMan/Extensions/UIColor+Colors.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+Colors.swift
+//  CoopMan
+//
+//  Created by Francis Breidenbach on 12/10/18.
+//  Copyright © 2018 Francis Breidenbach. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+  struct Project {
+    static let softPink = UIColor(red: 230, green: 198, blue: 198, alpha: 1.0)
+  }
+  
+}
